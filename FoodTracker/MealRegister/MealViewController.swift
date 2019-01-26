b6ce7ab7a4ea5580169115fdc248434126c014f1
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2019-01-26 18:57:29 -0200

    Creating list of meals

--- FoodTracker/MealRegister/MealViewController.swift
+++ FoodTracker/MealRegister/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Iury Miguel on 22/01/19.
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate,
+class MealViewController: UIViewController, UITextFieldDelegate,
 UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -46,7 +45,7 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     //Called after textField.resignFirstResponder()
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
+       
     }
     
     //MARK: UIImagePickerControllerDelegate
